9b3383b891f13ee281f54648929d5644dbb15a70
Linus Rachlis <linus@rachlis.net>
2015-04-30 08:12:09 -0400

    Comments/devnotes changes only

--- application/modules/wpq/Bootstrap.php
+++ application/modules/wpq/Bootstrap.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * This class's mere existence is necessary for the module autoloader
+ * to be automatically set up by Zend_Application.
+ */
 class Wpq_Bootstrap extends Zend_Application_Module_Bootstrap {
   
 }
