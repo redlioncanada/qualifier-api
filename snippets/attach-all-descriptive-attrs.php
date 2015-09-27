22f264d23de61dbdfbcd016854b9fd500dcaf9fb
Linus Rachlis <linus@rachlis.net>
2015-09-27 10:41:28 -0400

    Dev snippet: attach-all-descriptive-attrs.php

---
+++ snippets/attach-all-descriptive-attrs.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * For use in developing extraction logic for new category:
+ * attaches all descriptive attributes to output json under 'descr-attrs'
+ * property, to make it easy to search through them.
+ * 
+ * Comment out all categories in JsonBuilder other than the one being worked on
+ * to make this easier.
+ */
+
+foreach ($entry->getDescriptiveAttributeGroups() as $grpName => $grp) {
+  if (in_array($grpName, ['Endeca', 'EndecaProps'])) {
+    continue;
+  }
+  foreach ($grp->getDescriptiveAttributes() as $attr) {
+    $data['descr-attrs'][$grpName][] = [
+      'description' => $attr->description,
+      'valueidentifier' => $attr->valueidentifier,
+      'value' => $attr->value,
+      'noteinfo' => $attr->noteinfo,
+    ];
+  }
+}
