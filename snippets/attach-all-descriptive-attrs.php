345242fd072a91557509c09f7ed43785622f9e26
Linus Rachlis <linus@rachlis.net>
2015-10-13 21:33:30 -0400

    Just comments

--- snippets/attach-all-descriptive-attrs.php
+++ snippets/attach-all-descriptive-attrs.php
@@ -1,8 +1,10 @@
 <?php
 
 /*
- * For use in developing extraction logic for new category:
- * attaches all descriptive attributes to output json under 'descr-attrs'
+ * For use in developing extraction logic for new category: place in attachFeatureData()
+ * override in a subclass of Wpq\CatalogEntryProcessor\StandardAbstract.
+ *
+ * Attaches all descriptive attributes to output json under 'descr-attrs'
  * property, to make it easy to search through them.
  * 
  * Comment out all categories in JsonBuilder other than the one being worked on
