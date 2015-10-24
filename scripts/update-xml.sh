62d2cb50c31135d5ac50e55ca840152211893141
Linus Rachlis <lrachlis@k2digital.com>
2015-10-23 23:08:59 -0400

    Bash script for updating XML files from WP server

---
+++ scripts/update-xml.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# example usage:
+#   ./update-xml.sh MTGCA
+#
+
+cd ../data/source-xml &&\
+  wget -O xml_$1.zip "http://access.whirlpool.com/mr/getMediaType.do?mediaType=$1&sku=IBM_Extract" &&\
+  unzip xml_$1.zip &&\
+  mv $1/*.xml . &&\
+  rm -rf $1/ &&\
+  rm xml_$1.zip
